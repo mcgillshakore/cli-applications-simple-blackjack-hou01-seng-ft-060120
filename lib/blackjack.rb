afddda299fad3b68c7ac5f785baad0ffdee96b88
Shakore McGill <mcgills2093@gmail.com>
2020-05-29 20:02:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,7 +62,7 @@ def runner
     cards_total = initial_round
     cards_total= hit?(cards_total)
     display_card_total(cards_total)
-    if cards_total > 21
+    until cards_total > 21
       cards_total = hit?(cards_total)
       cards_total = display_card_total(cards_total)
     end
